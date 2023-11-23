4b1b3f0f62f739a2f171239f60ff47f06ebf8354
poncho_boncho <Sorkolas@mail.com>
2023-11-23 22:52:34 +0300

    Initial commit

---
+++ src/main/java/poncho_boncho/labs/laba2DI/Laba2DiApplication.java
@@ -0,0 +1,13 @@
+package poncho_boncho.labs.laba2DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba2DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba2DiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poncho_boncho/labs/laba2DI/Laba2DiApplicationTests.java
@@ -0,0 +1,13 @@
+package poncho_boncho.labs.laba2DI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba2DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
